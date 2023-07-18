71b3f662b01fb2bff7cd59a3ab0757746ac482da
naachopedraza1 <nachi_choco@live.com.ar>
2023-07-18 05:00:20 -0300

    slide

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,15 +10,13 @@ const HomePage = () => {
     <MainLayout bgClass='bg-main'>
 
       <Grid container minHeight="100vh" justifyContent="center" alignItems="center" className="bg-home">
-
         <Grid item xs={6} >
         </Grid>
-
         <Grid item xs={6}>
           <Grid container direction="column" gap={1}>
             <Image src="/miniilust.png" alt='Kill a server' width={115} height={20} />
             <Typography variant="h3" textTransform="uppercase" color="primary.main"> search and destroy </Typography>
-            <Typography variant="h6" mb={1}> Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. </Typography>
+            <Typography variant="h6" mb={2}>Explore our database of breached servers by our experts, we have the best and unique system penetration techniques, start destroying your competition.</Typography>
           </Grid>
           <Button variant='outlined' sx={{ mr: 2 }}>
             Learn more
@@ -27,15 +25,12 @@ const HomePage = () => {
             register
           </Button>
         </Grid>
-
       </Grid>
 
       <Grid container justifyContent="center" alignItems="center" className="bg-home">
-
         <Grid item xs={12} textAlign="center">
-          <Typography variant="h3" textTransform="uppercase" color="primary.main" mb={4}> we work on </Typography>
-
-
+          <Typography variant="h3" textTransform="uppercase" color="primary.main" mb={1}> we work on </Typography>
+          <span className='line'></span>
           <Slide slidesToShow={4} infinite>
             <Grid className="box-game each-slide-effect">
               <img src="/wow.png" width="100%" />
@@ -53,21 +48,22 @@ const HomePage = () => {
               <img src="/lineage2.png" width="100%" />
             </Grid>
           </Slide>
+        </Grid>
+      </Grid>
 
-          {/* <Grid container justifyContent="center" alignItems="center" gap={3}>
-            <Grid item xs={2.8} className="box-game">
-              <img src="/wow.png" width="100%" />
-            </Grid>
-            <Grid item xs={2.8} className="box-game">
-              <img src="/muonline.png" width="100%" />
-            </Grid>
-            <Grid item xs={2.8} className="box-game">
-              <img src="/aion.png" width="100%" />
-            </Grid>
-            <Grid item xs={2.8} className="box-game">
-              <img src="/cabal.png" width="100%" />
-            </Grid>
-          </Grid> */}
+      <Grid container justifyContent="center" alignItems="center" mt={5}>
+        <Grid item xs={6}>
+          <Grid container direction="column" gap={1}>
+            <Image src="/miniilust.png" alt='Kill a server' width={115} height={20} />
+            <Typography variant="h3" textTransform="uppercase" color="primary.main"> about us </Typography>
+            <Typography variant="h6" mb={2}> Our expertise is programming, executing exploits, creating DDOS attacks and cloning or extracting information from databases, we like the challenge of doing things where most others give up. </Typography>
+          </Grid>
+          <Button variant='outlined' sx={{ mr: 2 }}>
+            contact us
+          </Button>
+        </Grid>
+        <Grid item xs={6} textAlign="center">
+          <img src="/ilust-home2.png" width="80%" />
         </Grid>
       </Grid>
 
