3ba66d1301618d35728a981780a2ff21eeb71680
naachopedraza1 <nachi_choco@live.com.ar>
2023-08-03 01:24:23 -0300

    404 v3

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,10 +2,12 @@
 import { NextPage } from 'next'
 import { Grid, Typography } from '@mui/material';
 
-export const ErrorPage: NextPage = () => {
+const ErrorPage: NextPage = () => {
     return (
         <Grid container minHeight="100vh" justifyContent="center" alignItems="center">
             <Typography> {"dont even try, lol"} </Typography>
         </Grid>
     )
 }
+
+export default ErrorPage;
